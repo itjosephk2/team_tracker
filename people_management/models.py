c039bc4f1d0a9054b81aceb82b9f73ec5fe35a84
Joseph Keane <josephkeane996@gmail.com>
2024-10-20 16:02:16 +0000

    Created odels and added to admin

--- people_management/models.py
+++ people_management/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class person(models.Model):
+class Person(models.Model):
     first_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
     email = models.EmailField(max_length = 255)
@@ -10,15 +10,16 @@ class person(models.Model):
     active = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name} ({self.id})"
 
-class contract(models.Model):
-    person_id = models.ForeignKey(person, on_delete=models.CASCADE)
+class Contract(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
     job_title = models.CharField(max_length=255)
     contract_start = models.DateField()
-    contract_end = models.DateField()
+    contract_end = models.DateField(null=True, blank=True)
     hourly_rate = models.FloatField(default=12.45)
     contracted_hours = models.FloatField(default=40)
 
     def __str__(self):
-        return self.name
+        return f"{self.job_title}"
+        
