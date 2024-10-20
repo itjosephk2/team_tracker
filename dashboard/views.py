44ba634feca007eb4af9a85170554f1e789b5b90
Joseph Keane <josephkeane996@gmail.com>
2024-10-20 17:39:07 +0000

    set up base html for dashboard to fit into dashboard content as templates

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     return render(request, "dashboard/dashboard.html")
+
+def test(request):
+    return render(request, "dashboard/test.html")
