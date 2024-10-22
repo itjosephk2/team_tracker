c7d79d3f9baf352b3fedf59f94fbeb5301995dc1
Joseph Keane <josephkeane996@gmail.com>
2024-10-22 14:05:28 +0000

    switching to class based views

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from people_management.models import Person
+from django.views.generic import TemplateView
 
 # Create your views here.
-def dashboard(request):
+class DashboardView(TemplateView):
     people = Person.objects.all()
-    return render(request, 'dashboard/dashboard.html', {'people': people})
+    template_name = 'dashboard/dashboard.html'
+    extra_context = {'people': people}
+
+
 
     
