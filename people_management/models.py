af4b7f1692159e35af98b293c5a4209635def911
itjosephk2 <josephkeane996@gmail.com>
2025-03-01 13:34:12 +0000

    feat: adding a manager

--- people_management/models.py
+++ people_management/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from datetime import date
 
-
-
 # Create your models here.
 class Person(models.Model):
     first_name = models.CharField(max_length=50)
@@ -11,6 +9,13 @@ class Person(models.Model):
     phone_number = models.CharField(max_length=15)
     date_of_birth = models.DateField()
     active = models.BooleanField(default=False)  # Keep active stored in the DB
+    manager = models.ForeignKey(
+        'self',  # Self-referential ForeignKey
+        on_delete=models.SET_NULL,  # Keep employee if manager is removed
+        null=True,
+        blank=True,
+        related_name="team_members"
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.id})"
