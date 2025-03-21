53af7be783507bd62e1ad72f0fe0eb81edff1d8f
Joe Keane <josephkeane996@gmail.com>
2025-03-21 20:58:46 +0000

    feat: updating setting link

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,23 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let sidebar = document.getElementById("layoutSidenav_nav");
-    let sidebarToggle = document.getElementById("sidebarToggle");
-    let closeSidebar = document.getElementById("closeSidebar");
-
-    sidebarToggle.addEventListener("click", function() {
-        sidebar.classList.toggle("open");
-    });
-
-    closeSidebar.addEventListener("click", function() {
-        sidebar.classList.remove("open");
-    });
-
-    document.addEventListener("click", function(event) {
-        if (!sidebar.contains(event.target) && !sidebarToggle.contains(event.target)) {
-            sidebar.classList.remove("open");
-        }
-    });
-});
-
 // Sidebar toggle
 document.addEventListener("DOMContentLoaded", function () {
     console.log('Listening');
